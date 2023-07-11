bace05af7511bba5c48e0c129ec348bee3030ad2
Denis Sakhno <97180503+ideniska@users.noreply.github.com>
2023-07-10 20:16:28 -0400

    upd asgi

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,16 +1,38 @@
 """
-ASGI config for mysite project.
+ASGI config for core project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+"""Imports only after get_asgi_application() and env.
+Excluded error like:
+django.core.exceptions.AppRegistryNotReady: Apps aren't loaded yet.
+"""
+
+from app.routing import websocket_urlpatterns as progress_bar_urlpatterns
+from channels.auth import AuthMiddlewareStack
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(
+                    progress_bar_urlpatterns,
+                )
+            )
+        ),
+    }
+)
